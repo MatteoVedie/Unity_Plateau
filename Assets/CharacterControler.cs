be2f297835f245cc75d29fa6b171be1208284336
MatteoVedie <matteo.vedie2002@gmail.com>
2024-11-30 16:58:37 +0100

    UNITY fin

--- Assets/CharacterControler.cs
+++ Assets/CharacterControler.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CharacterControler : MonoBehaviour
+public class CustomCharacterController : MonoBehaviour
 {
     [SerializeField] private GameObject _target;
     private NavMeshAgent _agent;
     private Animator _animation;
+    private float _idleTimer; // Pour gérer les animations d'attente
+    private Rigidbody _rigidbody;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _agent.SetDestination(_target.transform.position);
         _animation = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         if (_agent != null && _animation != null)
         {
+            // Synchronise la vitesse avec l'Animator
             _animation.SetFloat("Speed", _agent.velocity.magnitude);
+
+            // Gérer les animations Idle aléatoires
+            if (_agent.velocity.magnitude < 0.1f) // Si le personnage est immobile
+            {
+                _idleTimer += Time.deltaTime;
+
+                if (_idleTimer > 3f) // Toutes les 3 secondes
+                {
+                    int randomIdle = Random.Range(0, 3); // Valeur aléatoire : 0, 1 ou 2
+                    _animation.SetInteger("IdleRandom", randomIdle);
+                    _idleTimer = 0; // Réinitialisation du timer
+
+                    
+                }
+            }
         }
     }
 }
